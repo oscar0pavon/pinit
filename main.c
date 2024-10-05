33e43c281e4f10723d1d1d0d8c8c54ce52257226
Oscar Pavon <pavon_oscar@outlook.com>
2024-10-05 22:29:58 +0000

    symlink, mount_threaded, ip addr

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <complex.h>
 #include <pthread.h>
 #define _XOPEN_SOURCE 200809L
 #include <signal.h>
@@ -10,24 +11,22 @@
 #include <sys/mount.h>
 
 #include <string.h>
-
+#define NULL 0
 #define TIMEO	30
 
 sigset_t set_of_signals;
 
+int symlink(const char *target, const char *linkpath);
+
 static char * const ls_command[] = {"/bin/ls",NULL};
 
-static char * const mount_proc_command[] = {"/bin/mount",
-  "-o", "nosuid,noexec,nodev", "-t", "proc", "proc", "/proc",NULL};
-static char * const mount_sys_command[] = {"/bin/mount",
-  "-o", "nosuid,noexec,nodev", "-t", "sysfs", "sys", "/sys",NULL};
-static char * const mount_dev_command[] = {"/bin/mount",
-  "-o", "mode=0775,nosuid", "-t", "devtmpfs", "dev", "/dev",NULL};
 
-static char* const ln_stdin_command[] = {"/bin/ln","-sf", "/proc/self/fd/0","/dev/stdin",NULL};
-static char* const ln_stdout_command[] = {"/bin/ln","-sf", "/proc/self/fd/1","/dev/stdout",NULL};
-static char* const ln_stderr_command[] = {"/bin/ln","-sf", "/proc/self/fd/2","/dev/stderr",NULL};
-static char* const ln_fd_command[] = {"/bin/ln","-sf", "/proc/self/fd","/dev/fd",NULL};
+static char * const mount_sys_commnad[] = {"sysfs","/sys", "sysfs"};
+
+static char * const mount_proc_commnad[] = {"proc","/proc", "proc"};
+static char * const mount_dev_commnad[] = {"dev","/dev", "devtmpfs"};
+static char * const mount_pts_commnad[] = {"devpts","/dev/pts", "devpts"};
+
 
 
 static char * const agetty_command[] = {"/sbin/agetty","--noclear", "--autologin", "root",
@@ -37,15 +36,16 @@ static char * const pts_command[] = {"/bin/mount","-n", "-t" , "devpts",
   "-o", "gid=5,mode=0620", "devpts", "/dev/pts", NULL};
 
 //wifi
+#define DEV "eth0"
 
-
-static char * const ip_set_up_command[] = {"/sbin/ip","link", "set" , "wlan0", 
+static char * const ip_set_up_command[] = {"/sbin/ip","link", "set" ,DEV, 
   "up", NULL};
 static char * const wpa_command[] = {"/sbin/wpa_supplicant","-B", "-c" , "/wifi", 
-  "-i", "wlan0", NULL};
+  "-i", DEV, NULL};
 static char * const ip_addr_command[] = {"/sbin/ip","addr", "add" , "192.168.0.23/24", 
-  "dev","wlan0", NULL};
-static char * const ip_route_command[] = {"/sbin/ip","route", "add","default","via", "192.168.0.1", "src", "192.168.0.23","dev" , "wlan0", NULL};
+  "dev",DEV, NULL};
+static char * const ip_route_command[] = {"/sbin/ip","route", "add","default","via",
+  "192.168.0.1", "src", "192.168.0.23","dev" , DEV, NULL};
 
 void wait_signal_for_close(int pid){
     
@@ -81,35 +81,23 @@ static void launch_agetty(){
 }
 
 
-void* mount_proc(void*){
-  int pid; 
-  if((pid = fork())){
-    wait_signal_for_close(pid);
-  }else{
-		setsid();
-		execvp(mount_proc_command[0],mount_proc_command );
-  } 
+struct MountCommand{
+  char * const *arguments;
+  unsigned long int mode; 
+};
+
+void* mount_threaded(void*command_line){
+  struct MountCommand* mount_command = (struct MountCommand*)command_line;
+
+  char* const* command = (char* const *)(mount_command->arguments);
+
+  mount(command[0], command[1], command[2],
+      mount_command->mode,NULL);
+
   return NULL;
 }
 
-void* mount_sys(void*){
-  int pid; 
-  if((pid = fork())){
-    wait_signal_for_close(pid);
-  }else{
-		setsid();
-		execvp(mount_sys_command[0],mount_sys_command );
-  }  
-  return NULL;
-}
 
-void* mount_dev(void*){
-  
-  
-  mount("dev", "/dev", "devtmpfs", MS_NOSUID,NULL);
-  printf("/dev mounted\n");
-  return NULL;
-}
 
 static bool signal_child = false;
 static void manage_signal(int sig){
@@ -175,37 +163,52 @@ int main(){
   pthread_t mount_thread;
   pthread_t mount_dev_thread;
 
-  pthread_create(&mount_thread, NULL , mount_proc, NULL) ;
+  
+  struct MountCommand mount_dev_struct; 
+  mount_dev_struct.arguments = mount_dev_commnad;
+  mount_dev_struct.mode = MS_NOSUID;
 
-  pthread_create(&mount_thread, NULL , mount_sys, NULL) ;
+  pthread_create(&mount_dev_thread, NULL , mount_threaded, &mount_dev_struct);
+  
+  struct MountCommand mount_proc_struct = {.arguments = mount_proc_commnad, 
+                                          .mode = MS_NOSUID | MS_NOEXEC | MS_NODEV}; 
+  
+  pthread_create(&mount_thread, NULL , mount_threaded, &mount_proc_struct);
+  
+  struct MountCommand mount_sys_struct = {.arguments = mount_sys_commnad, 
+                                          .mode = MS_NOSUID | MS_NOEXEC | MS_NODEV}; 
 
-  pthread_create(&mount_dev_thread, NULL , mount_dev, NULL) ;
- // mount_dev(NULL);
+  pthread_create(&mount_thread, NULL , mount_threaded, &mount_sys_struct);
+
+
   pthread_join(mount_dev_thread,NULL);
   
 
-  int mkdir_status;
-  mkdir_status = mkdir("/dev/pts", S_IRWXU | S_IRWXG | S_IRWXO);
-  if(mkdir_status == -1){
-    printf("Error creating /dev/pts\n");
-  }
 
-  pthread_create(&mount_thread, NULL , execute_thread_command, ln_stdin_command) ;
-  pthread_create(&mount_thread, NULL , execute_thread_command, ln_stdout_command) ;
-  pthread_create(&mount_thread, NULL , execute_thread_command, ln_stderr_command) ;
-  
-  pthread_create(&mount_thread, NULL , execute_thread_command, ln_fd_command) ;
 
-  pthread_create(&mount_thread, NULL , execute_thread_command, pts_command) ;
+  symlink("/proc/self/fd/0","/dev/stdin");
+  symlink("/proc/self/fd/1","/dev/stdout");
+  symlink("/proc/self/fd/2","/dev/stderr");
+  symlink("/proc/self/fd","/dev/fd");
+
+
  
+  mkdir("/dev/pts", S_IRWXU | S_IRWXG | S_IRWXO);
+  
+
+  struct MountCommand mount_pts_struct = {.arguments = mount_pts_commnad, 
+                                          .mode = 0}; 
+  //need chmod 0666 /dev/pts/ptmx
+  pthread_create(&mount_thread, NULL , mount_threaded, &mount_pts_struct);
+
 
   //wifi
-  //pthread_t ip_add_thread;
-// pthread_create(&mount_thread, NULL , execute_thread_command, ip_set_up_command) ;
+  pthread_t ip_add_thread;
+  pthread_create(&mount_thread, NULL , execute_thread_command, ip_set_up_command) ;
  // pthread_create(&mount_thread, NULL , execute_thread_command, wpa_command) ;
-  //pthread_create(&ip_add_thread, NULL , execute_thread_command, ip_addr_command) ;
-  //pthread_join(ip_add_thread,NULL);
-  //pthread_create(&mount_thread, NULL , execute_thread_command, ip_route_command) ;
+  pthread_create(&ip_add_thread, NULL , execute_thread_command, ip_addr_command) ;
+  pthread_join(ip_add_thread,NULL);
+  pthread_create(&mount_thread, NULL , execute_thread_command, ip_route_command) ;
 
   pthread_create(&mount_thread,NULL, test_sigchld, NULL);
 
